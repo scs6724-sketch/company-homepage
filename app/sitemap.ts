2948bd05372e7761f5f4fccec215c098d8f04994
scs6724-sketch <scs6724@google.com>
2026-05-04 14:28:12 +0900

    sitemap 추가

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://k-lights.net",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://k-lights.net/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://k-lights.net/products",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://k-lights.net/glass",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://k-lights.net/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
